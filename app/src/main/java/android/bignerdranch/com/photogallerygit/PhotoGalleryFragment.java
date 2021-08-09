47881d1a99b1bf133fa74cd20e063f9f3393060c
Tseop <77163842+tseop@users.noreply.github.com>
2021-08-09 14:32:19 +0900

    + PhotoGallery Upload
    23장 시작

---
+++ app/src/main/java/android/bignerdranch/com/photogallerygit/PhotoGalleryFragment.java
@@ -0,0 +1,11 @@
+package android.bignerdranch.com.photogallerygit;
+
+import androidx.fragment.app.Fragment;
+
+public class PhotoGalleryFragment extends Fragment {
+
+    public static PhotoGalleryFragment newInstance() {
+        return new PhotoGalleryFragment();
+    }
+
+}
