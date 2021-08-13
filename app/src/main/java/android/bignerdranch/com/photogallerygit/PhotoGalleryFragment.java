0f67fc9bdffab802db48c4addbd9059a86a587cf
tseop <77163842+tseop@users.noreply.github.com>
2021-08-13 10:08:59 +0900

    + PhotoGallery get JSON data for mac

--- app/src/main/java/android/bignerdranch/com/photogallerygit/PhotoGalleryFragment.java
+++ app/src/main/java/android/bignerdranch/com/photogallerygit/PhotoGalleryFragment.java
@@ -1,11 +1,56 @@
 package android.bignerdranch.com.photogallerygit;
 
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.IOException;
 
 public class PhotoGalleryFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private static final String TAG = "PhotoGalleryFragment";
 
     public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        new FetchItemTask().execute();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        mRecyclerView = view.findViewById(R.id.fragment_photo_gallery_recycler_view);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        return view;
+    }
+
+    private class FetchItemTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                String result = new FlickrFetchr().getUrlString("https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=85dfdfbe25078ac7e8c141252f6144c2&format=json&nojsoncallback=1");
+                Log.i(TAG, "Fetched contents of URL : " + result);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Failed to fetch URL : ", ioe);
+            }
+            return null;
+        }
+    }
 }
