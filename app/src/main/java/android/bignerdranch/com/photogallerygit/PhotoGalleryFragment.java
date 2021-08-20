fee47e778bcf3fdb914bed65a84011217a31aeec
Tseop <77163842+tseop@users.noreply.github.com>
2021-08-20 17:17:31 +0900

    + HandlerThread branch update
    핸들러 스레드 코드 작성 완료
    정상 작동 확인 Cal_DeskTop branch에만 업데이트 맥으로 코드 작성하여 main branch에 merge 예정

--- app/src/main/java/android/bignerdranch/com/photogallerygit/PhotoGalleryFragment.java
+++ app/src/main/java/android/bignerdranch/com/photogallerygit/PhotoGalleryFragment.java
@@ -1,14 +1,16 @@
 package android.bignerdranch.com.photogallerygit;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,28 +18,56 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PhotoGalleryFragment extends Fragment {
 
     private RecyclerView mRecyclerView;
+    private List<GalleryItem> mItems = new ArrayList<>();
+
     private static final String TAG = "PhotoGalleryFragment";
+
 
     public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
     }
+
+    private ThumbnailDownloader<PhotoHolder> mThumbnailDownloader;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
         new FetchItemTask().execute();
+
+        Handler responseHandler = new Handler();
+        mThumbnailDownloader = new ThumbnailDownloader<>(responseHandler);
+        mThumbnailDownloader.setThumbnailDownloadListener(
+                new ThumbnailDownloader.ThumbnailDownloadListener<PhotoHolder>() {
+                    @Override
+                    public void onThumbnailDownloaded(PhotoHolder target, Bitmap thumbnail) {
+                        Drawable drawable = new BitmapDrawable(getResources(), thumbnail);
+                        target.bindGalleryItem(drawable);
+                    }
+                });
+        mThumbnailDownloader.start();
+        mThumbnailDownloader.getLooper();
+        Log.i(TAG, "Background thread started");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mThumbnailDownloader.quit();
+        Log.i(TAG, "Background thread destroyed");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mThumbnailDownloader.clearQueue();
     }
 
     @Nullable
@@ -52,6 +82,7 @@ public class PhotoGalleryFragment extends Fragment {
 
     private class FetchItemTask extends AsyncTask<Void, Void, List<GalleryItem>> {
 
+
         @Override
         protected List<GalleryItem> doInBackground(Void... voids) {
             return new FlickrFetchr().fetchItems();
@@ -62,23 +93,32 @@ public class PhotoGalleryFragment extends Fragment {
             mItems = galleryItems;
             setupAdapter();
         }
+
     }
 
     private class PhotoHolder extends RecyclerView.ViewHolder {
+        //        private TextView mTitleTextView;
         private ImageView mItemImageView;
+
 
         public PhotoHolder(@NonNull View itemView) {
             super(itemView);
+//            mTitleTextView = (TextView) itemView;
             mItemImageView = (ImageView) itemView.findViewById(R.id.fragment_photo_gallery_image_view);
         }
+//        private void bindGalleryItem(GalleryItem item) {
+//            mTitleTextView.setText(item.toString());
+//        }
 
         private void bindGalleryItem(Drawable draw) {
             mItemImageView.setImageDrawable(draw);
         }
+
     }
 
     private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
         private List<GalleryItem> items;
+
 
         public PhotoAdapter(List<GalleryItem> item) {
             items = item;
@@ -87,26 +127,29 @@ public class PhotoGalleryFragment extends Fragment {
         @NonNull
         @Override
         public PhotoHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//            TextView textView = new TextView(getActivity());
+//            return new PhotoHolder(textView);
             LayoutInflater inflater = LayoutInflater.from(getActivity());
-            View view = inflater.inflate(R.layout.gallery_item, parent, false);
+            View view;
+            view = inflater.inflate(R.layout.gallery_item, parent, false);
             return new PhotoHolder(view);
         }
 
         @Override
         public void onBindViewHolder(@NonNull PhotoHolder holder, int position) {
             GalleryItem galleryItem = items.get(position);
+//            holder.bindGalleryItem(galleryItem);
             Drawable placeholder = getResources().getDrawable(R.drawable.bill_up_close);
             holder.bindGalleryItem(placeholder);
+            mThumbnailDownloader.queueThumbnail(holder, galleryItem.getUrl());
         }
-
 
         @Override
         public int getItemCount() {
             return items.size();
         }
-    }
 
-    private List<GalleryItem> mItems = new ArrayList<>();
+    }
 
     private void setupAdapter() {
         if (isAdded()) {
---
+++ app/src/main/java/android/bignerdranch/com/photogallerygit/ThumbnailDownloader.java
@@ -0,0 +1,102 @@
+package android.bignerdranch.com.photogallerygit;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class ThumbnailDownloader<T> extends HandlerThread {
+    private static final String TAG = "ThumbnailDownloader";
+
+    private static final int MESSAGE_DOWNLOAD = 0;
+
+    private Handler mRequestHandler;
+    private ConcurrentMap<T, String> mRequestMap = new ConcurrentHashMap<>();
+    private int i = 1;
+    private Handler mResponseHandler;
+    private ThumbnailDownloadListener<T> mThumbnailDownloadListener;
+
+    public interface ThumbnailDownloadListener<T> {
+        void onThumbnailDownloaded(T target, Bitmap thumbnail);
+    }
+
+    public void setThumbnailDownloadListener(ThumbnailDownloadListener<T> listener) {
+        mThumbnailDownloadListener = listener;
+    }
+
+    public ThumbnailDownloader(Handler responseHandler) {
+        super(TAG);
+        mResponseHandler = responseHandler;
+    }
+
+    public ThumbnailDownloader() {
+        super(TAG);
+
+    }
+
+    public void queueThumbnail(T target, String url) {
+        Log.i(TAG, "Got a URL : " + url);
+
+        if (url == null) {
+            mRequestMap.remove(target);
+        } else {
+            mRequestMap.put(target, url);
+            mRequestHandler.obtainMessage(MESSAGE_DOWNLOAD, target).sendToTarget();
+        }
+    }
+
+    @Override
+    protected void onLooperPrepared() {
+        mRequestHandler = new Handler() {
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                if (msg.what == MESSAGE_DOWNLOAD) {
+                    T target = (T) msg.obj;
+                    Log.i(TAG, "Got a request for URL : " + mRequestMap.get(target));
+                    handleRequest(target);
+                }
+            }
+        };
+    }
+
+    private void handleRequest(T target) {
+        try {
+            final String url = mRequestMap.get(target);
+            if (url == null) {
+                return;
+            }
+            byte[] bitmapBytes = new FlickrFetchr().getUrlBytes(url);
+            final Bitmap bitmap = BitmapFactory.decodeByteArray(bitmapBytes, 0, bitmapBytes.length);
+            Log.i(TAG, "Bitmap created");
+
+            mResponseHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    if (mRequestMap.get(target) != url) {
+                        Log.d("handleRequest", "내가 찍은 곳 중 오류 발생! - 현재 map의 데이터 값 : " + mRequestMap.get(target) + ", 우리가 요청한 url 값 : " + url);
+                        return;
+                    }
+                    Log.d("handleRequest", "내가 찍은 곳 " + mRequestMap.get(target) + ", i : " + i);
+                    i++;
+                    mRequestMap.remove(target);
+                    mThumbnailDownloadListener.onThumbnailDownloaded(target, bitmap);
+
+                }
+            });
+        } catch (IOException ioe) {
+            Log.e(TAG, "Error downloading image", ioe);
+        }
+    }
+
+    public void clearQueue() {
+        mRequestHandler.removeMessages(MESSAGE_DOWNLOAD);
+    }
+}
